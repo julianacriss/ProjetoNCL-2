944570edbdfab86bf24063b5134d4cc8f8defbe8
Juliana Cristina de Andrade <julianacristina.ti@gmail.com>
2015-01-15 21:18:21 -0200

    Versão 3.0
    Problemas:
    1) Só funciona o 1º comando por tecla - porém, está lendo correto por
    arquivo.
    2) Escolher tipos diferentes de comandos NCL.

--- script.lua
+++ script.lua
@@ -1,29 +1,48 @@
-function tratador (evt)
-  print("--> TRATADOR KEYS")
-  print(evt.key)
-	--[[if (evt.class == 'key') and (evt.type == 'press') then
-		print("--> TRATADOR KEYS")
-		print(evt.key)
-		if (evt.key == 'RED') then
-			event.post { 
-				class  = 'ncl',
-				type   = 'presentation',
-				action = 'stop',
-			}
-		elseif (evt.key == 'GREEN') then
-			event.post {
-				class  = 'ncl',
-				type   = 'presentation',
-				action = 'resume',
-			}
-		elseif (evt.key == 'YELLOW') then
-			event.post {
-				class  = 'ncl',
-				type   = 'presentation',
-				action = 'pause',
-			}
-		end	
-	end]]--
-end
+dofile("lib/LuaXML/xml.lua")
+dofile("lib/LuaXML/handler.lua")
 
-event.register(tratador)
+local function tratamento()
+  local evtGreen = {
+    class = 'ncl',
+    type  = 'attribution',
+    name  = 'green',
+    value = 1,
+  }
+  
+  local evtYellow = {
+    class = 'ncl',
+    type  = 'attribution',
+    name  = 'yellow',
+    value = 2,
+  }
+
+  while true do
+    local path = "/misc/ncl30/mpeg-u.xml"
+    local arquivo = io.open(path, "r")
+    if arquivo then
+      io.input(arquivo)
+      local content = io.read()
+      if (content ~= nil) then
+          print("--> CONTENT: ", content)
+          local xmlhandler = simpleTreeHandler()
+          local xmlparser = xmlParser(xmlhandler)
+          xmlparser:parse(content)      
+          print(xmlhandler.root['aui:HandGesture']._attr.gesture)          
+          if (xmlhandler.root['aui:HandGesture']._attr.gesture == 'hand_left') then
+            evtGreen.action = 'start'; event.post(evtGreen)
+            evtGreen.action = 'stop' ; event.post(evtGreen)
+          elseif (xmlhandler.root['aui:HandGesture']._attr.gesture == 'hand_right') then
+            evtYellow.action = 'start'; event.post(evtYellow)
+            evtYellow.action = 'stop' ; event.post(evtYellow)
+          end
+        io.close(arquivo)
+      os.remove(path)
+      end -- if content
+      
+    end --if arquivo
+    
+  end -- while
+end -- function
+
+
+event.timer(2000, tratamento)
